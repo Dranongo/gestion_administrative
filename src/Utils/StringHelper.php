7c8d95be7061f07c29df81c4a6bea278c0f3a68c
Maximilien Fonteyn <maximilien.fonteyn@gmail.com>
2019-03-29 15:17:41 +0100

    Add new Helper

---
+++ src/Utils/StringHelper.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Utils;
+
+/**
+ * This class contains method to convert and compare some strings in the form.
+ * This is a singleton with static methods. 
+ * Class StringHelper
+ * @package Utils
+ */
+class StringHelper
+{
+    /**
+     * @var StringHelper|null
+     */
+    private static $_instance = null;
+
+    /**
+     * StringHelper constructor.
+     */
+    final private function __construct() {}
+
+    /**
+     * Clone method is not allowed
+     */
+    final private function __clone()
+    {
+        throw new \Exception("Le clonage n'est pas autorisé");
+    }
+
+    /**
+     * @return StringHelper
+     */
+    public static function getInstance(): StringHelper
+    {
+        if (! self::$_instance instanceof StringHelper) {
+            self::$_instance = new static();
+        }
+
+        return self::$_instance;
+    }
+
+    /**
+     * 
+     *
+     * @param string $email
+     * @param boolean $required
+     * 
+     */
+    public static function checkEmail(string $email, bool $required = true)
+    {
+        return $required ? filter_var($email, FILTER_VALIDATE_EMAIL) : trim($email) == "" || filter_var($email, FILTER_VALIDATE_EMAIL);
+    } 
+}
