2285df6127603ad25f1c3bcf3a4274f735cb6605
Maximilien Fonteyn <maximilien.fonteyn@gmail.com>
2019-04-16 11:46:24 +0200

    Add "ContactUrgence" checking

--- src/Utils/FormHelper.php
+++ src/Utils/FormHelper.php
@@ -6,13 +6,20 @@ class FormHelper
     /**
      * @param array $formErrors
      * @param string $field
+     * @param string $fieldGroup
+     * @param string $index
      * @return string
      */
-    public static function setFeedbackInvalidity(array $formErrors, string $field, ?string $fieldGroup = ''): string
+    public static function setFeedbackInvalidity(array $formErrors, string $field, ?string $fieldGroup = '', ?string $index = ''): string
     {
         if ($fieldGroup == '') {
             return count($formErrors) != 0 && array_key_exists($field, $formErrors) ? $formErrors[$field] : '';
         } else {
+            if ($index != '') {
+                return count($formErrors) != 0 && array_key_exists($fieldGroup, $formErrors) 
+                    && array_key_exists($index, $formErrors[$fieldGroup])
+                    && array_key_exists($field, $formErrors[$fieldGroup][$index]) ? $formErrors[$fieldGroup][$index][$field] : '';
+            }
             return count($formErrors) != 0 && array_key_exists($fieldGroup, $formErrors) 
                 && array_key_exists($field, $formErrors[$fieldGroup]) ? $formErrors[$fieldGroup][$field] : '';
         }
@@ -21,13 +28,20 @@ class FormHelper
     /**
      * @param array $formErrors
      * @param string $field
+     * @param string $fieldGroup
+     * @param string $index
      * @return string
      */
-    public static function editFieldErrors(array $formErrors, string $field, ?string $fieldGroup = ''): string
+    public static function editFieldErrors(array $formErrors, string $field, ?string $fieldGroup = '', ?string $index = ''): string
     {
         if ($fieldGroup == '') {
             return count($formErrors) != 0 && array_key_exists($field, $formErrors) ? 'border border-danger' : '';
         } else {
+            if ($index != '') {
+                return count($formErrors) != 0 && array_key_exists($fieldGroup, $formErrors) 
+                    && array_key_exists($index, $formErrors[$fieldGroup])
+                    && array_key_exists($field, $formErrors[$fieldGroup][$index]) ? 'border border-danger' : '';
+            }
             return count($formErrors) != 0 && array_key_exists($fieldGroup, $formErrors) 
                 && array_key_exists($field, $formErrors[$fieldGroup]) ? 'border border-danger' : '';
         }
@@ -36,13 +50,20 @@ class FormHelper
     /**
      * @param array $formErrors
      * @param string $field
+     * @param string $fieldGroup
+     * @param string $index
      * @return string
      */
-    public static function editRadioFieldErrors(array $formErrors, string $field, ?string $fieldGroup = ''): string
+    public static function editRadioFieldErrors(array $formErrors, string $field, ?string $fieldGroup = '', ?string $index = ''): string
     {
         if ($fieldGroup == '') {
             return count($formErrors) != 0 && array_key_exists($field, $formErrors) ? 'text text-danger' : '';
         } else {
+            if ($index != '') {
+                return count($formErrors) != 0 && array_key_exists($fieldGroup, $formErrors) 
+                    && array_key_exists($index, $formErrors[$fieldGroup])
+                    && array_key_exists($field, $formErrors[$fieldGroup][$index]) ? 'text text-danger' : '';
+            }
             return count($formErrors) != 0 && array_key_exists($fieldGroup, $formErrors) 
                 && array_key_exists($field, $formErrors[$fieldGroup]) ? 'text text-danger' : '';
         }
--- views/salarie/formContactUrgence.html.php
+++ views/salarie/formContactUrgence.html.php
@@ -1,3 +1,4 @@
+        <?php use Utils\FormHelper; ?>
         <h2>Contact d'urgence</h2>
         <div class="form-group" id="contactsUrgence">
             <span class="addLine glyphicon glyphicon-plus"></span>
@@ -27,23 +28,31 @@
             <div class="form-row line">
                 <div class="form-group col-md-3">
                     <label>Nom *</label>
-                    <input type="text" class="form-control" 
-                        data-name="contact_urgence_form[%%d%%][nom]" value="">
+                    <input type="text" 
+                        class="form-control <?= FormHelper::editFieldErrors($formErrors, 'nom', 'contacts', '0') ?>" 
+                        name="contact_urgence_form[0][nom]" value="">
+                    <span><?= FormHelper::setFeedbackInvalidity($formErrors, 'nom', 'contacts', '0') ?></span>
                 </div> 
                 <div class="form-group col-md-3">
                     <label>Prénom *</label>
-                    <input type="text" class="form-control" 
-                        data-name="contact_urgence_form[%%d%%][prenom]" value="">
+                    <input type="text" 
+                        class="form-control <?= FormHelper::editFieldErrors($formErrors, 'prenom', 'contacts', '0') ?>"
+                        name="contact_urgence_form[0][prenom]" value="">
+                    <span><?= FormHelper::setFeedbackInvalidity($formErrors, 'prenom', 'contacts', '0') ?></span>
                 </div>
                 <div class="form-group col-md-3">
                     <label>Lien *</label>
-                    <input type="text" class="form-control" 
-                        data-name="contact_urgence_form[%%d%%][lien]" value="">
+                    <input type="text" 
+                        class="form-control <?= FormHelper::editFieldErrors($formErrors, 'lien', 'contacts', '0') ?>"
+                        name="contact_urgence_form[0][lien]" value="">
+                    <span><?= FormHelper::setFeedbackInvalidity($formErrors, 'lien', 'contacts', '0') ?></span>
                 </div>
                 <div class="form-group col-md-3">
                     <label>Téléphone *</label>
-                    <input type="text" class="form-control" 
-                        data-name="contact_urgence_form[%%d%%][telephone]" value="">
+                    <input type="text"
+                        class="form-control <?= FormHelper::editFieldErrors($formErrors, 'telephone', 'contacts', '0') ?>"
+                        name="contact_urgence_form[0][telephone]" value="">
+                    <span><?= FormHelper::setFeedbackInvalidity($formErrors, 'telephone', 'contacts', '0') ?></span>
                 </div>
             </div>
         </div>
